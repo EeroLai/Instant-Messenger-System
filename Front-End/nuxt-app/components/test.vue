b99023c973fcd3e404c5144dd4b8532a5487c776
EeroLai <84887419+EeroLai@users.noreply.github.com>
2022-10-03 20:37:16 +0800

    Update Framework

---
+++ Front-End/nuxt-app/components/test.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>{{config.apiBase}}</h1>
+</template>
+
+<script setup>
+const config = useRuntimeConfig()
+</script>
--- Front-End/nuxt-app/nuxt.config.ts
+++ Front-End/nuxt-app/nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    runtimeConfig: {
+        apiSecret: '', // can be overridden by NUXT_API_SECRET environment variable
+        public: {
+            apiBase: 'apiBase', // can be overridden by NUXT_PUBLIC_API_BASE environment variable
+        }
+    }
 })
+
