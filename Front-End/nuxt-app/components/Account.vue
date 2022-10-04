4f19aeb830983f4fbe949ec5f16e23d25c9fda48
EeroLai <84887419+EeroLai@users.noreply.github.com>
2022-10-04 21:04:19 +0800

    add text

--- Front-End/nuxt-app/components/Account.vue
+++ Front-End/nuxt-app/components/Account.vue
@@ -1,7 +1,19 @@
 <template>
     <div class="flex justify-center items-center h-screen">
         <div class="text-center">
-            test
+            <p class="text-2xl text-slate-200 font-black">Hellow</p>
+            <p class="text-2xl text-slate-200 font-black">Hellow</p>
+            <p class="text-2xl text-slate-200 font-black">Hellow</p>
+            <p class="text-2xl text-slate-200 font-black">Hellow</p>
+            <p class="text-2xl text-slate-200 font-black">Hellow</p>
+            <p class="text-2xl text-slate-200 font-black">Hellow</p>
+            <p class="text-2xl text-slate-200 font-black">Hellow</p>
+            <p class="text-2xl text-slate-200 font-black">Hellow</p>
+            <p class="text-2xl text-slate-200 font-black">Hellow</p>
+            <p class="text-2xl text-slate-200 font-black">Hellow</p>
+            <p class="text-2xl text-slate-200 font-black">Hellow</p>
+            <p class="text-2xl text-slate-200 font-black">Hellow</p>
+            <p class="text-2xl text-slate-200 font-black">Hellow</p>
         </div>
     </div>
 </template>
