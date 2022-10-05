d697b3b772de83ba9a8999617e34eb3354c4abfa
EeroLai <84887419+EeroLai@users.noreply.github.com>
2022-10-05 21:04:03 +0800

    fix signalR TestCode

--- Back-End/InstantMessengerApi/Hubs/ChatHub.cs
+++ Back-End/InstantMessengerApi/Hubs/ChatHub.cs
@@ -4,9 +4,13 @@ namespace InstantMessengerApi.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string msg)
+        public Task SendMessage(string msg)
         {
-            await Clients.All.SendAsync("ReceiveMsg", msg);
+            return Clients.All.SendAsync("ReceiveMsg", msg);
+        }
+        public async Task SendMessage2(string msg)
+        {
+            await Clients.All.SendAsync("ReceiveMsg2", msg + "SendMessage2");
         }
     }
 }
